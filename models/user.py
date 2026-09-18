6fe2113b14464719c44e68343a3c59a955cd62ce
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-18 13:03:12 +0530

    user model

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from database import Base
+
+
+class user(Base):
+    __tablename__= "users"
+
+    id = Column(Integer, index=True, primary_key=True)
+    name = Column(String, index=True, unique=True)
+    email = Column(String, index=True, unique=True)
+    password_hash = Column(String)
+    created_at  = Column(DateTime)
