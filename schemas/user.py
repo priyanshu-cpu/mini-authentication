99e19f4b38c83d55b0f699511653ecb71dfbe4ff
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-18 13:08:41 +0530

    user schema

---
+++ schemas/user.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    username: str
+    email: Optional[EmailStr] = None
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+    
