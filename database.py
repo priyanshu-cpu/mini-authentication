934b75ccc1930c5e6700cdc846356c1a7c104a00
priyanshu-cpu <priyanshurajput12863@gmail.com>
2026-09-18 12:50:22 +0530

    db conn config

--- database.py
+++ database.py
@@ -1,2 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from utils.settings import settings
+
+engine = create_engine(settings.DB_CONNECTION)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
